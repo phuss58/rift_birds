d7eda6364f3e68bed7787a8d57610b6939ef72cd
phuss58 <hussp@lopers.unk.edu>
2024-09-27 16:47:09 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,6 @@
 # load bcftools
 module load bcftools
+
+# ex
+
+vcftools --vcf ${workdir}/04_vcf/CM001988.1.vcf --max-missing 1.0 --min-alleles 2 --max-alleles 2 --max-maf 0.49 --mac 2 --recode --recode-INFO-all --out ${workdir}/test.file
