2b95b32f6a5711e57382207bb11e442b13771fa6
phuss58 <hussp@lopers.unk.edu>
2024-10-07 17:01:00 -0500

    Admixture and Structure

--- admixture.sh
+++ admixture.sh
@@ -1,3 +1,9 @@
+admixture=/common/cooperlab/phuss58/albertine/06_plink/dist/admixture_linux-1.3.0/admixture
+
+
+
 # running admixture
 
-for K in 3; do admixture --cv ${workdir}/05_structure/5kbpthin_plink.ped $K; done
+for K in 1 2 3 4 5 6; do $admixture --cv ${workdir}/05_structure/5kbpthin_plink.ped $K; done
+
+for K in 1 2 3 4 5 5 6; do $admixture --cv ${workdir}/06_plink/nothin_plink.ped $K | tee log${K}.out; done
--- shortcuts.sh
+++ shortcuts.sh
@@ -1,3 +1,5 @@
 # shortcuts
 
 admixture=/common/cooperlab/phuss58/albertine/06_plink/dist/admixture_linux-1.3.0/admixture
+
+workdir=/common/cooperlab/phuss58/albertine/
